1877d4b802b11322c0de997c24685c053311306e
Anton <stormantonio.che@gmail.com>
2016-10-18 21:45:03 +0300

    lesson1

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int age;
+        age = 18;
+        double rost = 1.8;
+        String boy = "Коле";
+        System.out.println(boy + " уже " + age + " лет, он ростом " + rost + " метра");
+    }
+}
