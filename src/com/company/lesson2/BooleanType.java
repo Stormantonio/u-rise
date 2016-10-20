d5a42e1bd840de88476f3e63f3123bc07e6ed00c
Anton <stormantonio.che@gmail.com>
2016-10-21 00:16:58 +0300

    lesson3

--- src/com/company/lesson2/BooleanType.java
+++ src/com/company/lesson2/BooleanType.java
@@ -9,7 +9,8 @@ public class BooleanType {
 
     public static void main(String[] args) {
 
-        int ageMisha = 21;
+        // Мой код
+        /*int ageMisha = 21;
         int ageVasya = 25;
         int ageVadim = 17;
 
@@ -45,7 +46,50 @@ public class BooleanType {
 
         System.out.println("Oldest man - " + max);
         System.out.println("Middle age man - " + middle);
-        System.out.println("Most young man - " + mostYounger);
-    }
+        System.out.println("Most young man - " + mostYounger);/*/
 
+
+        // код Стаса
+        Integer dimaAge = 35;
+        Integer mishaAge = 50;
+        Integer vasyaAge = 65;
+
+        Integer oldest = 0;
+        Integer youngest = 0;
+        Integer middle = 0;
+
+        if (dimaAge <= mishaAge && dimaAge <= vasyaAge) {
+            youngest = dimaAge;
+            if (mishaAge <= vasyaAge) {
+                middle = mishaAge;
+                oldest = vasyaAge;
+            } else {
+                middle = vasyaAge;
+                oldest = mishaAge;
+            }
+        }
+        if (mishaAge <= dimaAge && mishaAge <= vasyaAge) {
+            youngest = mishaAge;
+            if (dimaAge <= vasyaAge) {
+                middle = dimaAge;
+                oldest = vasyaAge;
+            } else {
+                middle = vasyaAge;
+                oldest = dimaAge;
+            }
+        }
+        if (vasyaAge <= dimaAge && vasyaAge <= mishaAge) {
+            youngest = vasyaAge;
+            if (dimaAge <= mishaAge) {
+                middle = dimaAge;
+                oldest = mishaAge;
+            } else {
+                middle = mishaAge;
+                oldest = dimaAge;
+            }
+        }
+        System.out.println("Oldest man - " + oldest);
+        System.out.println("Middle age man - " + middle);
+        System.out.println("Most young man - " + youngest);
+    }
 }
