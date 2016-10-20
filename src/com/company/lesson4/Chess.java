cb099052b0dca2f75e40fd21dccae0498489c584
Anton <stormantonio.che@gmail.com>
2016-10-21 00:18:58 +0300

    lesson4

--- src/com/company/lesson4/Chess.java
+++ src/com/company/lesson4/Chess.java
@@ -6,31 +6,33 @@ package com.company.lesson4;
 public class Chess {
     public static void main(String[] args) {
 
+        System.out.println("Мое решение:");
         String[] rows = {"a", "b", "c", "d", "e", "f", "g", "h"};
-        String[] columns = {"1", "2", "3", "4", "5", "6", "7"};
-
-        String[][] chessCells = new String[8][8];
-
-
-        for (int i = 0; i < rows.length; i++) {
-
-            for (int j = 0; j < columns.length; j++) {
-                chessCells[i][j] = rows[i] + columns[j];
-
-                System.out.print(chessCells[i][j]);
-
-                break;
-            }
-                /*if (columns[j] == columns[columns.length - 1]) {
-                    System.out.println();
-                    System.out.println("_____");*/
-                if (rows[i] == rows[rows.length - 1]) {
-                    System.out.println();
-                    System.out.println("________________");
+        String[] columns = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        String[][] chessCells = new String[rows.length][columns.length];
+        for (int i = columns.length - 1; i >= 0; i--) {
+            for (int j = 0; j < rows.length; j++) {
+                chessCells[j][i] = rows[j] + columns[i];
+                System.out.print("|" + chessCells[j][i]);
+                if (rows[j] == rows[rows.length - 1]) {
+                    System.out.println("|");
+                    System.out.println("________________________");
                 }
-
             }
         }
 
+        System.out.println();
+        System.out.println("Решение с вебинара:");
+        String[][] chessBoard = new String[8][8];
+        String letters = "abcdefgh";
+        for (int i = 0; i < chessBoard.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < chessBoard[i].length; j++) {
+                chessBoard[i][j] = "" + letters.charAt(j) + (8 - i) + "|";
+                System.out.print(chessBoard[i][j]);
+            }
+            System.out.println("\n-------------------------");
+        }
+
     }
-//}
+}
