8c2ca541c5d6ea421fca28442ad38493710d2b88
Anton <stormantonio.che@gmail.com>
2016-10-21 00:20:04 +0300

    lesson1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         int age;
         age = 18;
         double rost = 1.8;
