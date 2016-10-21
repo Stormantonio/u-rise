77231e2d4e29f736553ac0463aec445f9a48a2e2
Anton <stormantonio.che@gmail.com>
2016-10-21 18:59:26 +0300

    lesson3

--- src/com/company/lesson3/Cycles.java
+++ src/com/company/lesson3/Cycles.java
@@ -5,10 +5,16 @@ package com.company.lesson3;
  */
 public class Cycles {
     public static void main(String[] args) {
-        int count = 10;
+        /*int count = 10;
         for (int i = 1; i <= count; i++) {
             System.out.println(i + " негритят из-за стола вышло");
             System.out.println(count - i + " негритят за столом осталось");
+        }*/
+
+        int blackCount = 10;
+        int blackSize = blackCount;
+        for (int i = 0; i < blackSize; i++) {
+            System.out.println("Вышло из-за стола " + i + " За столом сидело " + blackCount--);
         }
     }
 }
